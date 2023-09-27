2e960632fabcf1a847f7f301c5cb4f59f5360d8f
Vmukgoma/atHome <koonnokhook212@gmail.com>
2023-09-28 03:18:30 +0700

    add star rating and image slider / fixed layout draft 1 / delete 5th store

---
+++ resources/js/star_rating.js
@@ -0,0 +1,36 @@
+const stars = document.querySelectorAll('.star');
+const averageRating = document.getElementById('average');
+
+let currentRating = 0;
+
+// Add click event listeners to each star
+stars.forEach(star => {
+  star.addEventListener('click', () => {
+    const rating = parseInt(star.getAttribute('data-rating'));
+
+    // Reset all stars to their default color
+    stars.forEach(s => {
+      s.style.color = 'black';
+    });
+
+    // Color the selected stars
+    for (let i = 0; i < rating; i++) {
+      stars[i].style.color = 'gold';
+    }
+
+    currentRating = rating;
+    updateAverageRating();
+  });
+});
+
+function updateAverageRating() {
+  // Calculate the average rating and update the display
+  const totalRatings = stars.length;
+  let sum = 0;
+  stars.forEach(star => {
+    sum += parseInt(star.getAttribute('data-rating'));
+  });
+
+  const average = sum / totalRatings;
+  averageRating.textContent = average.toFixed(2);
+}
